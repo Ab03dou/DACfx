c1e3a0f4a3afeac82f259dcd3c31a38939ae1af5
aymen-arch <aymendev45@gmail.com>
2024-12-13 16:34:22 +0100

    rigt python path ll win

--- src/main/java/com/example/imgclassapp/controler/PythonScriptExecutor.java
+++ src/main/java/com/example/imgclassapp/controler/PythonScriptExecutor.java
@@ -8,10 +8,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PythonScriptExecutor {
-    private static final String VENV_PYTHON = System.getProperty("user.home") + "/su/bin/python";  // for Linux/Mac
-    // For Windows use: System.getProperty("user.home") + "/su/Scripts/python.exe"
 
     public static String executePythonScript(String scriptPath, String... args) {
+
+        String VENV_PYTHON;
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            VENV_PYTHON = "C:\\Users\\"+ System.getProperty("user.home") + "\\AppData\\Local\\Programs\\Python\\Python310\\python.exe";  // for Windows
+        } else {
+            VENV_PYTHON = System.getProperty("user.home") + "/su/bin/python";  // for Linux/Mac
+        }
         try {
             List<String> commands = new ArrayList<>();
             commands.add(VENV_PYTHON);
